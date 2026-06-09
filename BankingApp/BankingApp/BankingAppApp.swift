b0553aed367884f266dbb6f0938d8ce736907758
Amrit Purandare <amrutpurandare213@gmail.com>
2026-06-09 15:26:42 +0530

    feat: initial project structure

---
+++ BankingApp/BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Devayani Purandare on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
