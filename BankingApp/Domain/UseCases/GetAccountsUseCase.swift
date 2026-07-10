218bb9bbfeed9cbad20acd47b68dde00981224ed
devayanidahake <devayani.dahake@gmail.com>
2026-07-10 13:05:32 +0530

    feat(domain): add get accounts use case

--- BankingApp/Domain/UseCases/GetAccountsUseCase.swift
+++ BankingApp/Domain/UseCases/GetAccountsUseCase.swift
@@ -1,7 +1,23 @@
 //
-//  FetchAccountsUseCase.swift
+//  GetAccountsUseCase.swift
 //  BankingApp
 //
 //  Created by Devayani Purandare on 10/07/26.
 //
+import Foundation
 
+protocol GetAccountsUseCase {
+    func execute() async throws -> [Account]
+}
+
+final class DefaultGetAccountUseCase : GetAccountsUseCase, Sendable {
+    private let repository: any DashboardRepository
+    
+    init(repository: any DashboardRepository) {
+        self.repository = repository
+    }
+    
+    func execute() async throws -> [Account] {
+        try await repository.fetchAccounts()
+    }
+}
