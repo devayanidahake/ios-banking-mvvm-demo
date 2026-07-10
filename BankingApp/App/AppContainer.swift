f87359b7e5d1c6fcfc3f0ea4399e10037bcbb536
devayanidahake <devayani.dahake@gmail.com>
2026-07-10 16:58:33 +0530

    refactor(app): register dashboard dependencies

--- BankingApp/App/AppContainer.swift
+++ BankingApp/App/AppContainer.swift
@@ -6,29 +6,32 @@
 //
 import Foundation
 
-final class AppContainer {
-
+final class AppContainer{
     let apiClient: any APIClient
-
+    
+    private lazy var dashboardRepository: any DashboardRepository = {
+        DefaultDashboardRepository(apiClient: apiClient)
+    }()
+    
+    private lazy var getAccountUseCase: any GetAccountsUseCase = {
+       DefaultGetAccountUseCase(repository: dashboardRepository)
+    }()
+    
     init() {
-
         let configuration = NetworkConfiguration.production
-
         let session = URLSession.shared
-
-        let requestBuilder = RequestBuilder(
-            configuration: configuration
-        )
-
+        
+        let requestBuilder = RequestBuilder(configuration: configuration)
+        
         let responseValidator = ResponseValidator()
-
+        
         let decoder = JSONDecoderFactory.makeDefaultDecoder()
-
-        self.apiClient = URLSessionAPIClient(
-            session: session,
-            requestBuilder: requestBuilder,
-            responseValidator: responseValidator,
-            decoder: decoder
-        )
+        
+        self.apiClient = URLSessionAPIClient(session: session, requestBuilder: requestBuilder, responseValidator: responseValidator, decoder: decoder)
     }
+    
+    func  makeDashboardViewModel() -> DashboardViewModel {
+        DashboardViewModel(getAccountUseCase: getAccountUseCase)
+    }
+    
 }
